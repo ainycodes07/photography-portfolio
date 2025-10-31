da60841cb3d163f27d76d5f66cb137e00dc42791
Qurat Ul Ain <197101652+ainycodes07@users.noreply.github.com>
2025-10-31 14:34:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById("menu-toggle");
+const nav = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  toggle.textContent = nav.classList.contains("active") ? "✕" : "☰";
+});
